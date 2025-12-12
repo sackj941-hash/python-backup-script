c82abba61940ca594913d5ac7e5552fe632b0329
Badal BK <your-email@example.com>
2025-12-12 20:40:28 +1100

    Add calculator script

---
+++ calculator.py
@@ -0,0 +1,20 @@
+num1 = int(input ("enter first number: "))
+num2 = int(input ("enter second number:"))
+
+print(type(num1))
+print(type(num2))
+sum_of_num = num1 + num2
+
+print("sum of 2 numbers: ",sum_of_num) 
+
+diff_of_num = num1 - num2
+
+print("difference of 2 numbers: ",diff_of_num)
+
+prod_of_num = num1 * num2
+
+print("product of 2 numbers: ",prod_of_num)
+
+div_of_num = num1 / num2
+
+print("division of 2 numbers: ",div_of_num)
